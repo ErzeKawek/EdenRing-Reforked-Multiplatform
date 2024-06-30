f0d2cc4140d32d2cbcecfeea0d0067368c21ee1c
ErzeKawek <130212655+ErzeKawek@users.noreply.github.com>
2024-06-30 11:17:36 +0700

    First Commit Templating

---
+++ common/src/main/java/paulevs/edenring/ExampleMod.java
@@ -0,0 +1,9 @@
+package paulevs.edenring;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "edenring";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
